8b702800cbb84bfc2738485f11a5c1883b0ea686
Adrian Valentim <valentimadrian@gmail.com>
2022-02-22 22:20:16 -0300

    Update core.clj

--- src/clojure_spellchecker/core.clj
+++ src/clojure_spellchecker/core.clj
@@ -1,5 +1,13 @@
 (ns clojure-spellchecker.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
+(def word-list (slurp "resources/english-words.txt"))
+
+(str/split-lines word-list)
+
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
